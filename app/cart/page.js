e04fa6ca1bfadd07febe7b9d24a966b5b7662ed8
acttcha <acttcha@gmail.com>
2023-09-22 01:11:56 +0900

    add cart

---
+++ app/cart/page.js
@@ -0,0 +1,36 @@
+import {age, name} from './data.js'
+import hello from './hello.js'
+export default function Cart() {
+    let 장바구니 = ['Tomatoes', 'Pasta', 'ㅇㅇ']
+    return (
+        
+      <div>
+        {hello}
+        <h4 className="title">Cart</h4>
+        <CartItem item={장바구니[0]}/>
+        <CartItem item={장바구니[1]}/>
+        <CartItem item={장바구니[2]}/>
+        <Banner content="롯데카드"></Banner>
+        <Banner content="현대카드"></Banner>
+
+        
+
+      </div>
+      
+
+    )
+  } 
+
+  function Banner (props){
+    return <h5>{props.content} 결제 행사중</h5>
+  }
+
+  function CartItem(props){
+    return (
+        <div className="cart-item">
+          <p>{props.item}</p>
+          <p>$40</p>
+          <p>1개</p>
+        </div>
+    )
+  }
